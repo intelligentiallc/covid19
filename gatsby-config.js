b65088103419c63346c143aa246c823293dff028
Mina <you@example.com>
2020-03-29 09:46:58 -0400

    add prefex

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+    { pathPrefix: "/covid19" },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
