dcd1370e5aee6f3999a6bfdb952648e1cca259c3
Mina <you@example.com>
2020-03-29 09:56:31 -0400

    add

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
+  pathPrefix: "/covid19" ,
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
   plugins: [
-    { pathPrefix: "/covid19" },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
